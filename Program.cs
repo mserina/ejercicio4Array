49861db1eedb76c555c4f1e6efa37af4708a01be
DESKTOP-ALP9J2M\Usuario <csi23-mserina@colegioaltair.net>
2023-12-22 16:27:49 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ejercicio4Array
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int tamaño;
+            Console.WriteLine("Indice el tamaño del vector");
+            tamaño = Convert.ToInt32(Console.ReadLine());
+            
+            //Creacion de la Array
+            int[] numeros = new int[tamaño];
+            Random numerosRandom = new Random();
+
+            //Asignar numeros enteros aleatorios a cada posicion del Array
+            for (int i = 0;i < numeros.Length;i++)
+            {
+                numeros[i] = numerosRandom.Next(1, 100);
+                Console.WriteLine(numeros[i]);
+            }
+
+            Array.Sort(numeros);
+            Console.WriteLine("Ascendente");
+            foreach (int listaNumeros in numeros)
+            {
+                Console.WriteLine(listaNumeros);
+            }
+           
+           
+        }
+    }
+}
